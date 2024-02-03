aa7deca861e70ad926505429b02ddd26acd63b57
zachary chapman <zach.chapman24@gmail.com>
2024-02-02 23:45:20 -0500

    app.get for index/notes

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const app = express();
 const path = require('path')
 const PORT = process.env.port || 3001;
 
+
+//For landing page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 });
 
+//For notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+});
+
+//
+app.get('/', (req, res) => {
+ 
+});
+
+//PORT
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
